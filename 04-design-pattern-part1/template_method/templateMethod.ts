8fd9aeef79ccd8fe7136266af9864651f03d017b
TakayukiHirano117 <ikusa.hirano@icloud.com>
2025-11-30 14:19:24 +0900

    Template Method

--- 04-design-pattern-part1/template_method/templateMethod.ts
+++ 04-design-pattern-part1/template_method/templateMethod.ts
@@ -1,2 +1,46 @@
 export {}
 
+abstract class TestTemplate {
+  test() {
+    this.setup()
+    this.execute()
+    this.teardown()
+  }
+
+  abstract setup();
+  abstract execute();
+
+  teardown() {
+    console.log('teardown')
+  }
+}
+
+class ItemServiceTest extends TestTemplate {
+  setup() {
+    console.log('setup: ItemServiceTest')
+  }
+
+  execute() {
+    console.log('execute: ItemServiceTest')
+  }
+}
+
+class UserServiceTest extends TestTemplate {
+  setup() {
+    console.log('setup: UserServiceTest')
+  }
+
+  execute() {
+    console.log('execute: UserServiceTest')
+  }
+}
+
+function run() {
+  const itemServiceTest = new ItemServiceTest();
+  itemServiceTest.test();
+
+  const userServiceTest = new UserServiceTest();
+  userServiceTest.test();
+}
+
+run();
